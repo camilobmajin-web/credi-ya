b44b47f9e50044008ebb6bf2c8ebbfff66e662fb
Camilo Blandon <camilobmajin@gmail.com>
2026-04-10 00:59:55 +0200

    Quitar .next del repo

--- .next/dev/static/chunks/app_page_tsx_12uwdpq._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_067ybjh._.js","static/chunks/node_modules_0mx1iuk._.js"],
-    source: "dynamic"
-});
