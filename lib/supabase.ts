a8ef469334b65f87fb36af86e13f0a6b46ff02c1
Camilo Blandon <camilobmajin@gmail.com>
2026-04-10 14:46:23 +0200

    Quitar lock de auth en supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://akepioaeqghyxrzwtwej.supabase.co";
-const supabaseKey = "sb_publishable_-IG7SX5FAIeMEQO1J0brCw_wJaXWHcC";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;"https://akepioaeqghyxrzwtwej.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;"sb_publishable_-IG7SX5FAIeMEQO1J0brCw_wJaXWHcC";
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false,
+  },
+});
